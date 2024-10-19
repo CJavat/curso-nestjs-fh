1a1e0472f32043bced0879afaf44121538b604ea
CJavat <cdpm98@hotmail.com>
2024-10-19 11:08:30 -0600

    SECCIÓN 6 - TERMINADO

--- 02-my-dashboard/src/app/dashboard/pokemons/[name]/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemons/[name]/page.tsx
@@ -1,23 +1,29 @@
 import { Metadata } from "next";
 import Image from "next/image";
-import { PokemonResponse } from "@/pokemons";
+import { PokemonResponse, PokemonsResponse } from "@/pokemons";
 import { notFound } from "next/navigation";
 
 interface Props {
-  params: { id: string };
+  params: { name: string };
 }
 
 export async function generateStaticParams(props: Props) {
   console.log(props);
-  const static151Pokemons = Array.from({ length: 151 }).map(
-    (pokemon, index) => `${index + 1}`
-  );
-  return static151Pokemons.map((id) => ({ id }));
+
+  const data: PokemonsResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=151`
+  ).then((response) => response.json());
+
+  const static151Pokemons = data.results.map((pokemon) => ({
+    name: pokemon.name,
+  }));
+
+  return static151Pokemons.map(({ name }) => ({ name }));
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   try {
-    const { id, name } = await getPokemon(params.id);
+    const { id, name } = await getPokemon(params.name);
 
     return {
       title: `Pokémon ${id} - ${name.toUpperCase()}`,
@@ -34,9 +40,9 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   }
 }
 
-const getPokemon = async (id: string): Promise<PokemonResponse> => {
+const getPokemon = async (name: string): Promise<PokemonResponse> => {
   const pokemon: PokemonResponse = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${id}`,
+    `https://pokeapi.co/api/v2/pokemon/${name}`,
     {
       cache: "force-cache",
     }
@@ -51,7 +57,7 @@ const getPokemon = async (id: string): Promise<PokemonResponse> => {
 };
 
 export default async function PokemonPage({ params }: Props) {
-  const pokemon = await getPokemon(params.id);
+  const pokemon = await getPokemon(params.name);
 
   return (
     <div className="flex mt-5 flex-col items-center text-slate-800">
