1a883f78ad508d021e83ffd08f853a9f14e9fdab
CJavatX <cdpm98@hotmail.com>
2024-10-25 14:05:15 -0600

    SECCIÓN 10 - TERMINADA

--- 03-admin-todos/src/app/api/todos/route.ts
+++ 03-admin-todos/src/app/api/todos/route.ts
@@ -43,3 +43,18 @@ export async function POST(request: Request) {
     return NextResponse.json(error, { status: 400 });
   }
 }
+
+export async function DELETE(request: Request) {
+  try {
+    await prisma.todo.deleteMany({
+      where: { complete: true },
+    });
+
+    return NextResponse.json({
+      message: "Las tareas completadas han sido eliminadas",
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(error, { status: 400 });
+  }
+}
---
+++ 03-admin-todos/src/app/dashboard/rest-todos/page.tsx
@@ -0,0 +1,21 @@
+import prisma from "@/lib/prisma";
+import { NewTodo, TodosGrid } from "@/todos";
+
+export const metadata = {
+  title: "Listado de Todos",
+  description: "Se muestran todos los todos",
+};
+
+export default async function RestTodosPage() {
+  const todos = await prisma.todo.findMany({ orderBy: { description: "asc" } });
+
+  return (
+    <div>
+      <div className="w-full px-5 pb-5">
+        <NewTodo />
+      </div>
+
+      <TodosGrid todos={todos} />
+    </div>
+  );
+}
