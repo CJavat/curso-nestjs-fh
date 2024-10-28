70eb57ebe0b127f51f776dccad19f9b5cc7776d3
CJavat <cdpm98@hotmail.com>
2024-10-28 07:03:38 -0600

    SECCIÓN 13 / CAP 15 - EN CURSO

--- 03-admin-todos/src/app/api/auth/[...nextauth]/route.ts
+++ 03-admin-todos/src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,17 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { Adapter } from "next-auth/adapters";
 
+import prisma from "@/lib/prisma";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import TwitterProvider from "next-auth/providers/twitter";
 import FacebookProvider from "next-auth/providers/facebook";
 
 export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
+    // Configure one or more authentication providers
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
@@ -26,6 +30,37 @@ export const authOptions: NextAuthOptions = {
     }),
     // ...add more providers here
   ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      console.log({ user });
+      console.log({ account });
+      console.log({ profile });
+
+      return true;
+    },
+
+    async jwt({ token, user, account, profile }) {
+      const dbUser = await prisma.user.findUnique({
+        where: { email: token.email ?? "no-email" },
+      });
+
+      token.roles = dbUser?.roles ?? ["no-roles"];
+      token.id = dbUser?.id ?? ["no-uuid"];
+
+      return token;
+    },
+
+    async session({ session, token, user }) {
+      if (session && session.user) {
+        session.user.roles = token.roles;
+        session.user.id = token.id;
+      }
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
