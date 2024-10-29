35bce9c3ce79bf559cac8f5a3f01d0882b62d385
CJavatX <cdpm98@hotmail.com>
2024-10-29 11:53:26 -0600

    SECCIÓN 16 - TERMINADO

---
+++ 04-teslo-shop/src/seed/seed-database.ts
@@ -0,0 +1,49 @@
+import prisma from "../lib/prisma";
+
+import { initialData } from "./seed";
+
+async function main() {
+  // 1. Borrar registors previos.
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+
+  const { categories, products } = initialData;
+
+  // 2. Categorías
+  const categoriesData = categories.map((name) => ({ name }));
+  await prisma.category.createMany({ data: categoriesData });
+
+  const categoriesDB = await prisma.category.findMany();
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+
+    return map;
+  }, {} as Record<string, string>);
+
+  // 3. Productos
+  products.forEach(async (product) => {
+    const { images, type, ...rest } = product;
+
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type],
+      },
+    });
+
+    const imagesData = images.map((image) => ({
+      url: image,
+      productId: dbProduct.id,
+    }));
+    await prisma.productImage.createMany({ data: imagesData });
+  });
+}
+
+(() => {
+  if (process.env.NODE_ENV === "production") return;
+
+  main();
+
+  console.log("Seed Ejecutado Correctamente");
+})();
