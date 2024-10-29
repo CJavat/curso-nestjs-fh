57a4b310ae37ed21b2fe014e8d829f65d5866512
CJavat <cdpm98@hotmail.com>
2024-10-28 18:44:14 -0600

    SECCIÓN 15 / CAP 6 - EN CURSO

--- 04-teslo-shop/src/app/(shop)/category/[id]/page.tsx
+++ 04-teslo-shop/src/app/(shop)/category/[id]/page.tsx
@@ -1,20 +1,41 @@
 import { notFound } from "next/navigation";
+import { ProductGrid, Title } from "@/components";
+
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 
 interface Props {
   params: {
-    id: string;
+    id: Category;
   };
 }
 
+const seedProducts = initialData.products;
+
 export default function CategoryPage({ params }: Props) {
   const { id } = params;
-  if (id !== "kids") {
-    notFound();
-  }
+
+  const products = seedProducts.filter((product) => product.gender === id);
+  const labels: Record<Category, string> = {
+    men: "Hombres",
+    women: "Mujeres",
+    kid: "Niños",
+    unisex: "Todos",
+  };
+
+  // if (id !== "kid" || id !== "men" || id !== "women") {
+  //   notFound();
+  // }
 
   return (
-    <div>
-      <h1>Hello {id}</h1>
-    </div>
+    <>
+      <Title
+        title={labels[id]}
+        subtitle={`Artículos Para ${labels[id]}`}
+        className="mb-2"
+      />
+
+      <ProductGrid products={products} />
+    </>
   );
 }
