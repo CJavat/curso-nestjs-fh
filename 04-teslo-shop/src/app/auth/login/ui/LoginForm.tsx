717dd0553b8e907536a2726420ef0baadf5703f0
CJavatX <cdpm98@hotmail.com>
2024-10-31 15:00:14 -0600

    SE AGREGARON NUEVOS PROVEEDORES DE AUTENTICACIÓN

--- 04-teslo-shop/src/app/auth/login/ui/LoginForm.tsx
+++ 04-teslo-shop/src/app/auth/login/ui/LoginForm.tsx
@@ -4,9 +4,15 @@ import { useActionState, useEffect } from "react";
 import { useFormStatus } from "react-dom";
 import Link from "next/link";
 import clsx from "clsx";
-import { IoCheckmarkDoneOutline, IoInformationOutline } from "react-icons/io5";
+import {
+  IoCheckmarkDoneOutline,
+  IoInformationOutline,
+  IoLogoFacebook,
+  IoLogoGoogle,
+} from "react-icons/io5";
 
 import { authenticate } from "@/actions";
+import { signIn } from "next-auth/react";
 
 export const LoginForm = () => {
   const [state, dispatch] = useActionState(authenticate, undefined);
@@ -17,6 +23,14 @@ export const LoginForm = () => {
       window.location.replace("/");
     }
   }, [state]);
+
+  const loginWithGoogle = async () => {
+    await signIn("google");
+  };
+
+  const loginWithFacebook = async () => {
+    await signIn("facebook");
+  };
 
   return (
     <form action={dispatch} className="flex flex-col">
@@ -54,20 +68,34 @@ export const LoginForm = () => {
         )}
       </div>
 
-      {/* <button type="submit" className="btn-primary">
-        Ingresar
-      </button> */}
       <LoginButton />
 
-      {/* divisor l ine */}
       <div className="flex items-center my-5">
         <div className="flex-1 border-t border-gray-500"></div>
         <div className="px-2 text-gray-800">O</div>
         <div className="flex-1 border-t border-gray-500"></div>
       </div>
 
-      <Link href="/auth/new-account" className="btn-secondary text-center">
-        Crear una nueva cuenta
+      <div className="flex justify-around items-center">
+        <button
+          onClick={loginWithGoogle}
+          type="button"
+          className="w-fit border border-black hover:border-white text-black hover:text-white hover:bg-black rounded-md p-2"
+        >
+          <IoLogoGoogle size={30} />
+        </button>
+        <button
+          onClick={loginWithFacebook}
+          type="button"
+          className="w-fit border border-black hover:border-white text-black hover:text-white hover:bg-black rounded-md p-2"
+        >
+          <IoLogoFacebook size={30} />
+        </button>
+      </div>
+
+      <Link href="/auth/new-account" className="my-2 btn-secondary text-center">
+        ¿No tienes una cuenta?,{" "}
+        <span className="text-blue-500"> Crear una nueva cuenta</span>
       </Link>
     </form>
   );
--- 04-teslo-shop/src/auth.config.ts
+++ 04-teslo-shop/src/auth.config.ts
@@ -1,28 +1,17 @@
+import { z } from "zod";
+import bcrypt from "bcryptjs";
+
 import NextAuth, { type NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import { z } from "zod";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+
 import prisma from "./lib/prisma";
-import bcrypt from "bcryptjs";
 
 export const authConfig: NextAuthConfig = {
   pages: {
     signIn: "/auth/login",
     newUser: "/auth/new-account",
-  },
-  callbacks: {
-    jwt({ token, user }) {
-      if (user) {
-        token.data = user;
-      }
-
-      return token;
-    },
-    session({ session, token, user }) {
-      console.log(user);
-      session.user = token.data as any;
-
-      return session;
-    },
   },
   providers: [
     Credentials({
@@ -51,7 +40,92 @@ export const authConfig: NextAuthConfig = {
         return rest;
       },
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
+
+      async profile(profile) {
+        const userExists = await prisma.user.findUnique({
+          where: { email: profile.email },
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            role: true,
+            emailVerified: true,
+            image: true,
+          },
+        });
+        if (userExists) return userExists;
+
+        const user = await prisma.user.create({
+          data: {
+            name: profile.name,
+            email: profile.email,
+            password: bcrypt.hashSync("password", 10),
+            image: profile.picture,
+          },
+        });
+
+        const { password: _, ...rest } = user;
+        console.log(_);
+
+        return {
+          ...rest,
+        };
+      },
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID ?? "",
+      clientSecret: process.env.FACEBOOK_SECRET ?? "",
+
+      async profile(profile) {
+        const userExists = await prisma.user.findUnique({
+          where: { email: profile.email },
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            role: true,
+            emailVerified: true,
+            image: true,
+          },
+        });
+        if (userExists) return userExists;
+
+        const user = await prisma.user.create({
+          data: {
+            name: profile.name,
+            email: profile.email,
+            password: bcrypt.hashSync("password", 10),
+            image: profile.picture.data.url,
+          },
+        });
+
+        const { password: _, ...rest } = user;
+        console.log(_);
+
+        return {
+          ...rest,
+        };
+      },
+    }),
   ],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.data = user;
+      }
+
+      return token;
+    },
+    session({ session, token, user }) {
+      console.log(user);
+      session.user = token.data as any;
+
+      return session;
+    },
+  },
 };
 
 export const { signIn, signOut, auth, handlers } = NextAuth(authConfig);
